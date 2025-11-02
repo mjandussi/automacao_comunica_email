688fc1e9d97e36ddf4c6d3dd2e3c10fa72f12101
mjandussi <112095881+mjandussi@users.noreply.github.com>
2025-11-02 13:36:08 -0300

    .

--- scheduler.py
+++ scheduler.py
@@ -47,7 +47,7 @@ def run_scraper():
 
 #### Agendando a função para os horários desejados
 schedule.every().day.at("09:00").do(run_scraper)
-schedule.every().day.at("13:31").do(run_scraper)
+schedule.every().day.at("12:00").do(run_scraper)
 schedule.every().day.at("15:00").do(run_scraper)
 schedule.every().day.at("18:00").do(run_scraper)
 
